47b2df5294cb56cd7bc3907d877f008b787d8861
frapimoneto <frapimoneto@gmail.com>
2020-07-02 17:45:34 -0300

    images uploads

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,15 +1,23 @@
 import { Router } from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
-import createUserService from '../services/CreateUserService';
+import CreateUserService from '../services/CreateUserService';
+import UpdateAvatarUserService from '../services/UpdateAvatarService';
+
 // Rota: Receber a requisição, chamar outro arquivo, devolver uma resposta
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated'
+import UpdateUserAvatarService from '../services/UpdateAvatarService';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async(request, response) => {
   try {
     const { name, email, password } = request.body;
 
-    const createUser = new createUserService();
+    const createUser = new CreateUserService();
 
     const user = await createUser.execute({
       name,
@@ -24,5 +32,22 @@ usersRouter.post('/', async(request, response) => {
     return response.status(400).json({ error: err.message });
   }
 });
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+  try{
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      avatarFilename: request.file.filename,
+    });
+
+    delete user.password;
+
+    return response.json(user);
+  }catch(err) {
+    return response.status(400).json({ error: err.message });
+  }
+}) //patch se usa para alterar uma única informação ao contrário do put 
 
 export default usersRouter;
